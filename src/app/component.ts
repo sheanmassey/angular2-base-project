5551305e8fa4e81507ee9cde79ec4990894e5cd5
sheanmassey <shean.massey@gmail.com>
2016-10-26 18:31:51 +0200

    added missing app.component

---
+++ src/app/component.ts
@@ -0,0 +1,12 @@
+import {Component, Input, OnInit, AfterViewInit} from '@angular/core'
+import {Router} from '@angular/router'
+
+
+@Component({
+    selector: 'app',
+    templateUrl: '/static/templates/app/component.html',
+})
+export class AppComponent {
+    
+}
+
