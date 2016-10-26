6c4945d806c252d70ec75dc1a71b12066d412898
sheanmassey <shean.massey@gmail.com>
2016-10-26 17:24:13 +0200

    updating from 2.0 to 2.1

---
+++ src/app/module.ts
@@ -0,0 +1,40 @@
+import {NgModule} from '@angular/core'
+import {BrowserModule} from '@angular/platform-browser'
+import {RouterModule} from '@angular/router'
+
+// import {AppComponent} from './component'
+// import {HomeComponent} from '../home.component'
+
+
+// import * as d3 from 'd3'
+// import 'c3'
+// var c = c3.generate({data: {}});
+//c.data;
+// var d = d3.ascending(21, 2);
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot([
+            // {
+            //     path: 'home',
+            //     component: HomeComponent,
+            // },
+            // {
+            //     path: '**',
+            //     redirectTo: '/error404',
+            // },
+        ]),
+    ],
+    declarations: [
+        // AppComponent,
+        // HomeComponent,
+    ],
+    bootstrap: [
+        // AppComponent
+    ],
+})
+export class AppModule {
+    
+}
+
