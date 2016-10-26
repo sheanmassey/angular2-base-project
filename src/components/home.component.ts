65eeb38b3d85c9bd55404d79d06a03a28cd4e429
sheanmassey <shean.massey@gmail.com>
2016-10-26 18:27:05 +0200

    added missing home component

---
+++ src/components/home.component.ts
@@ -0,0 +1,11 @@
+import {Component, Input, OnInit, AfterViewInit} from '@angular/core'
+import {Router} from '@angular/router'
+
+
+@Component({
+    selector: 'home',
+    templateUrl: '/static/templates/components/home.html',
+})
+export class HomeComponent {
+    
+}
